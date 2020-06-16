83db69a3d7199e8859cd5f2613f1bb5a432616ad
fabianriewe <f.riewe@riewe-software.de>
2020-06-16 12:07:11 +0200

    improved options

--- cli.py
+++ cli.py
@@ -4,12 +4,10 @@ import json
 
 cli = click.Group()
 
-BASE_URL = "http://127.0.0.1:5000"
 
-
-def login(email, password):
+def login(email, password, endpoint):
     click.echo("Logging in..")
-    response = requests.post(BASE_URL + "/login", json={"email": email, "password": password})
+    response = requests.post(endpoint + "/login", json={"email": email, "password": password})
     if response.ok:
         click.echo("Successfully logged in!")
         return response.json()
@@ -18,14 +16,14 @@ def login(email, password):
         raise click.Abort()
 
 
-def post_event_file(organization, workspace, event_file, token):
+def post_event_file(organization, workspace, event_file, token, endpoint):
     click.echo("Updating the event file...")
     json_data = {
         'workspace_name': workspace,
         'organization_name': organization,
         'event_file': event_file
     }
-    response = requests.post(BASE_URL + "/event_file", json=json_data,
+    response = requests.post(endpoint + "/event_file", json=json_data,
                              headers={'Authorization': 'Bearer {}'.format(token)})
     if response.ok:
         click.echo("Successfully updated the event file!")
@@ -37,12 +35,13 @@ def post_event_file(organization, workspace, event_file, token):
 
 @cli.command()
 @click.argument('event_file', type=click.File())
-@click.option('-e', type=str, help="Your email")
-@click.option('-p', type=str, help="Your password")
-@click.option('-w', type=str, help="Your workspace name")
-@click.option('-o', type=str, help="Your organization name")
-def load(event_file, e, p, w, o):
+@click.option('-e', '--email', type=str, help="Your email")
+@click.option('-p', '--password', type=str, help="Your password")
+@click.option('-w', '--workspace', type=str, help="Your workspace name")
+@click.option('-o', '--organization', type=str, help="Your organization name")
+@click.option('--endpoint', type=str, help="Eventhub endpoint", default="http://127.0.0.1:5000")
+def load(event_file, email, password, workspace, organization, endpoint):
     event_file_content = json.loads(event_file.read())
 
-    token = login(e, p)
-    post_event_file(o, w, event_file_content, token)
+    token = login(email, password, endpoint)
+    post_event_file(organization, workspace, event_file_content, token, endpoint)
